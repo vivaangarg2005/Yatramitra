9c8d9ddcba001afedfa3995d1f58636addd320c5
Vivaan Garg <vivaangarg2005@gmail.com>
2025-09-21 11:28:32 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,43 @@
+import { useEffect } from "react";
+import "./App.css";
+import Hero from "./components/Hero";
+import Navbar from "./components/navbar";
+import Features from "./components/Features";
+import HowItWorks from "./components/HowItWorks";
+import Impact from "./components/Impact";
+import Footer from "./components/Footer";
+import DestinationCard from "./components/Destination";
+import HiddenGems from "./components/HiddenGems";
+
+function App() {
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, []);
+
+  return (
+    <div>
+      <Navbar />
+      <div id="home">
+        <Hero />
+      </div>
+      <div id="destination">
+        <DestinationCard />
+      </div>
+      <div id="hiddengems">
+        <HiddenGems />
+      </div>
+      <div id="features">
+        <Features />
+      </div>
+      <div id="howitworks">
+        <HowItWorks />
+      </div>
+      <div id="impact">
+        <Impact />
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
